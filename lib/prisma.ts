442920a16796f4dad3e4f16b66a59915ddfd3680
Senuka Chandunu <senukaacc@gmail.com>
2026-02-08 11:39:39 +0530

    fix: prisma issues

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -2,6 +2,7 @@ import { PrismaClient } from '@/lib/generated/prisma'
 import { PrismaPg } from '@prisma/adapter-pg'
 import { Pool } from 'pg'
 
+
 const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined
 }
